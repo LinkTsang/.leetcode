49950772d1d7e9ae0d5879ec6b381b73253c4365
Link <linkdoyle@163.com>
2020-10-27 20:42:12 +0800

    Add Helper.

---
+++ solutions/helper.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2020 LinkD
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+import os
+import sys
+from typing import *
+from collections import *
+
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def assertEqual(actual, excepted):
+  if actual != excepted:
+    raise RuntimeError(f'Excepted {excepted}, got {actual}')
