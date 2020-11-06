a84d10871f70a5da3cf55dea3c2e0ed1b049ca30
Link <linkdoyle@163.com>
2020-11-06 20:51:09 +0800

    Sync.

---
+++ solutions/1402.做菜顺序.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=1402 lang=python3
+#
+# [1402] 做菜顺序
+#
+from helper import *
+# @lc code=start
+
+
+class Solution:
+  def maxSatisfaction(self, satisfaction: List[int]) -> int:
+    satisfaction.sort()
+    if satisfaction[-1] <= 0:
+      return 0
+    ans = 0
+    s = 0
+    dp = 0
+    for i in reversed(range(len(satisfaction))):
+      dp += s + satisfaction[i]
+      s += satisfaction[i]
+      ans = max(ans, dp)
+
+    return ans
+
+
+# @lc code=end
+assertEqual(Solution().maxSatisfaction([-1, -8, 0, 5, -9]), 14)
+assertEqual(Solution().maxSatisfaction([4, 3, 2]), 20)
+assertEqual(Solution().maxSatisfaction([-1, -4, -5]), 0)
+assertEqual(Solution().maxSatisfaction([-2, 5, -1, 0, 3, -3]), 35)
